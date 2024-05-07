993ccc7a571fd4411d36cf77ab2edcceba418a0a
1 <a1@MacBook-Pro-1.local>
2024-05-07 15:34:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name="kurmanbek";
+        student.dateOfBirth=22;
+        student.phoneNumber=79770000;
+        student.nationality="Kyrggyz";
+        Student student1=new Student("kurmanbek",22,777777,"kyrgyz");
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    int dateOfBirth;
+    int phoneNumber;
+    String nationality;
+
+    public Student() {
+    }
+
+    public Student(String name, int dateOfBirth, int phoneNumber, String nationaly) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.nationality = nationaly;
+    }
+}
